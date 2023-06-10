f88a4139e328e35d55d9bdf3d7748d50bd6ddf5c
Seasawher <tellagoodstory@icloud.com>
2023-06-10 11:51:37 +0000

    いろいろ追加

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,11 @@
-console.log('hello deno!');
+import { Command } from "https://deno.land/x/cliffy@v0.25.7/command/mod.ts";
+
+await new Command()
+  .name("Hello Cliffy")
+  .version("0.1.0")
+  .description("Hello Cliffy と出力するだけのCLIツールです")
+  .arguments("<args>")
+  .action((_option, ..._args) => {
+    console.log("Hello Cliffy!!");
+  })
+  .parse(Deno.args);
